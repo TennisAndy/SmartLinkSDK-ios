1525b012bfd305e27198ca711986d9e47490bba3
Andy <logsoul@163.com>
2026-07-06 11:48:49 +0800

    1.更新WunuLockLib编译库

--- WunuLockLib.framework/Headers/WunuLockLib.h
+++ WunuLockLib.framework/Headers/WunuLockLib.h
@@ -6,7 +6,15 @@
 //  Copyright © 2019年 WUNU. All rights reserved.
 //
 
+#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#else
+#import <Foundation/Foundation.h>
+#endif
+#else
+#import <Foundation/Foundation.h>
+#endif
 
 //! Project version number for WunuLockLib.
 FOUNDATION_EXPORT double WunuLockLibVersionNumber;
@@ -15,5 +23,3 @@ FOUNDATION_EXPORT double WunuLockLibVersionNumber;
 FOUNDATION_EXPORT const unsigned char WunuLockLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WunuLockLib/PublicHeader.h>
-
-
